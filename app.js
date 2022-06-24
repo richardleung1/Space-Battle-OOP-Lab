68015bc678e1bf0655fa33d7721a2604f164421d
richardleung1 <richardl57549@gmail.com>
2022-06-24 11:25:17 -0400

    create app.js and add instructions

---
+++ app.js
@@ -0,0 +1,31 @@
+// Space Battle OOP Lab
+
+// Build a game of battling alien spaceships using Javascript.
+
+// Earth has been attacked by a horde of aliens! You are the captain of the USS Assembly, on a mission to destroy every last alien ship.
+
+// Battle the aliens as you try to destroy them with your lasers.
+
+// There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.
+
+// Ship Properties
+// hull is the same as hitpoints. If hull reaches 0or less, the ship is destroyed
+// firepower is the amount of damage done to the hull of the target with a successful hit
+// accuracy is the chance between 0 and 1 that the ship will hit its target
+
+
+
+// Your spaceship, the USS Assembly should have the following properties:
+
+// hull - 20
+// firepower - 5
+// accuracy - .7
+
+
+
+// The alien ships should each have the following ranged properties determined randomly:
+
+// hull - between 3and 6
+// firepower - between 2and 4
+// accuracy - between .6and .8
+
